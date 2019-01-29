81b51afebb087c655e2c4a3ec2e4eacfdbdd96f9
Guillaume GARDET <guillaume.gardet@arm.com>
2019-01-29 09:09:45 +0100

    arm: Move initrd upper to leave more space for kernel
    This patch allows to have bigger kernels. If the kernel grows, then it will
    overwrite the initrd when it is extracted.
    
    Signed-off-by: Guillaume GARDET <guillaume.gardet@arm.com>
    Acked-by: Alexander Graf <agraf@suse.de>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>
    (cherry picked from commit 748e544fd0eb5fb618c4613f69612bc8c2eb65b6)

--- include/grub/arm/linux.h
+++ include/grub/arm/linux.h
@@ -28,7 +28,7 @@
 #if defined GRUB_MACHINE_UBOOT
 # include <grub/uboot/uboot.h>
 # define LINUX_ADDRESS        (start_of_ram + 0x8000)
-# define LINUX_INITRD_ADDRESS (start_of_ram + 0x02000000)
+# define LINUX_INITRD_ADDRESS (start_of_ram + 0x03000000)
 # define LINUX_FDT_ADDRESS    (LINUX_INITRD_ADDRESS - 0x10000)
 # define grub_arm_firmware_get_boot_data grub_uboot_get_boot_data
 # define grub_arm_firmware_get_machine_type grub_uboot_get_machine_type
